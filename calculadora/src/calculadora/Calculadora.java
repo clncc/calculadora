41ffdf55889f4a503924a2627f7ade913c458663
Joan de Canyamars <Joan de Canyamars@LAPTOP-PTSS6KH0>
2021-03-15 17:00:36 +0100

    proyecto calculadora

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Joan de Canyamars
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = 2;
+        int numero2 = 5;
+        
+        System.out.println(sumar(numero, numero2));
+    }
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
